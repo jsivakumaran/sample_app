7d278224ec3096419a9838ef18356807f0f88c63
Jay Sivakumaran <jsivakumaran@outlook.com>
2015-09-29 23:48:47 -0700

    Add Empty Subtitle case handling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #Returns the title based on weather the sub-title is provided or not - key is the veritcal backtrace
+  def full_title(page_title = '')
+    base_title = "SampleApp"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
